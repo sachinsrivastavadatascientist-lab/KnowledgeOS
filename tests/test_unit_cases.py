2c07b5315052a5d786662ccfd3237833a96f3dab
sachinsrivastavadatascientist-lab <sachinsrivastavadatascientist@gmail.com>
2026-06-30 23:25:53 +0530

    test_6

--- tests/test_unit_cases.py
+++ tests/test_unit_cases.py
@@ -1,10 +1,13 @@
 # tests/test_unit_cases.py
 
 import pytest
+import pytest
+
 from fastapi.testclient import TestClient
 from api.main import app   # or your FastAPI entrypoint
 
 client = TestClient(app)
+
 
 def test_home():
     response = client.get("/")
